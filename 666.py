7497c61506e5626e0b2824d1bb9bbd8ba1d08668
winterke988 <2860520760@qq.com>
2025-02-18 15:27:39 +0800

    Update 666.py

--- 666.py
+++ 666.py
@@ -133,10 +133,3 @@ if st.button("Predict"):
 
           
     st.write(advice)
-# Calculate SHAP values and display force plot
-import shap
-explainer = shap.TreeExplainer(rfc)
-shap_values = explainer.shap_values(pd.DataFrame([feature_values], columns=feature_names))
-shap.force_plot(explainer.expected_value[0], shap_values[0], pd.DataFrame([feature_values], columns=feature_names), matplotlib=True)
-plt.savefig("shap_force_plot.png", bbox_inches='tight', dpi=1200)
-st.image("shap_force_plot.png")
